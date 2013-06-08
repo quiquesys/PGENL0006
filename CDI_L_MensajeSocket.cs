6c5aae6bc3184276417a05e3982f23f80dd7c2b7
quiquesys <quiquesys@gmail.com>
2013-06-08 10:44:35 -0600

    Versión estable para el envío por sockets

---
+++ CDI_L_MensajeSocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace PGENL0006
+{
+    public class CDI_L_MensajeSocket
+    {
+        public void CDI_P_Enviar(TcpClient cliente, String Dato)
+        {
+            try
+            {
+                NetworkStream netstream = cliente.GetStream();
+                StreamWriter escribir = new StreamWriter(netstream);
+                escribir.WriteLine(Dato);
+                escribir.Flush();
+            }
+            catch (Exception) { }
+        }
+        public String CDI_F_Recibir(TcpClient cliente)
+        {
+            try
+            {
+                NetworkStream netstream = cliente.GetStream();
+                StreamReader leer = new StreamReader(netstream);
+
+                return leer.ReadLine();
+            }
+            catch (Exception) { return ""; }
+        }
+    }
+}
